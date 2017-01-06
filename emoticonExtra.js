daf54e13c88280ae3cccb612ef7fc2ec5eb3aae7
WiiUGuy <gamecheater@yahoo.com>
2017-01-06 17:00:21 -0600

    Made infinite running script
    javascript:(function(){var s='https://raw.githubusercontent.com/WiiUGuy/emoteExtra/master/emoticonExtra.js';(typeof frames[1]==='object'?frames[0]:window).document.body.appendChild(s);})();

--- emoticonExtra.js
+++ emoticonExtra.js
@@ -1,4 +1,5 @@
 (function();{ 
+ while (i == 0);{
  In = getDocumentById('demo')
  document.In.innerHTML = document.In.innerHTML.replace(":weegee:", "<img src='http://3dsplaza.com/chathnd/weegee.png'>");
-}
+}} 
