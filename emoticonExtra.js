0248c90d22c8ee1ad5b1b41e64bc2dd658c29ee1
WiiUGuy <gamecheater@yahoo.com>
2017-01-06 16:34:25 -0600

    emoticonExtra.js
    Currently, the Weegee emoticon is available.

---
+++ emoticonExtra.js
@@ -0,0 +1,6 @@
+(function();{
+while (i == 0);{
+ In = getDocumentById('demo')
+ document.In.innerHTML = document.In.innerHTML.replace(":weegee:", "<img src='http://3dsplaza.com/chathnd/weegee.png'>");
+}
+}
